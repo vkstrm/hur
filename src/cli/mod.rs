64a6bc322e688677fb7f3f46fb094d001aabfe1a
vkstrm <mongomoves@gmail.com>
2022-11-12 16:13:09 +0100

    Remove --query-header flag
    Better to use jq or whatever

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,12 +1,12 @@
 use std::io::Read;
 
-use super::http::{Method, headers::Headers};
+use super::http::{headers::Headers, Method};
 use super::logs;
 
-use crate::error::Error;
 use crate::error;
+use crate::error::Error;
 
-use clap::{ArgMatches, Arg, crate_version, crate_authors, crate_name};
+use clap::{crate_authors, crate_name, crate_version, Arg, ArgMatches};
 
 pub mod output;
 use output::Output;
@@ -16,7 +16,7 @@ pub struct Input {
     pub method: Method,
     pub headers: Headers,
     pub body: Option<String>,
-    pub allow_proxy: bool
+    pub allow_proxy: bool,
 }
 
 pub fn parse_args(args: Vec<String>) -> Result<(Input, Output), Error> {
@@ -33,11 +33,12 @@ pub fn parse_args(args: Vec<String>) -> Result<(Input, Output), Error> {
 fn parse_input(matches: &ArgMatches) -> Result<Input, Error> {
     let mut headers = headers(&matches)?;
     let body = collect_body(
-        matches.value_of("body"), 
-        matches.value_of("json"), 
-        &mut headers)?;
+        matches.value_of("body"),
+        matches.value_of("json"),
+        &mut headers,
+    )?;
 
-    Ok(Input{
+    Ok(Input {
         url: matches.value_of("url").unwrap().to_string(),
         method: get_method(matches.value_of("method").unwrap()),
         headers,
@@ -49,7 +50,6 @@ fn parse_input(matches: &ArgMatches) -> Result<Input, Error> {
 fn parse_output(matches: &ArgMatches) -> Output {
     Output {
         verbose: matches.is_present("verbose"),
-        query_header: matches.value_of("query-header").map(|q| String::from(q.trim())),
         no_body: matches.is_present("no-body"),
     }
 }
@@ -72,8 +72,8 @@ fn single_headers(headers_option: Option<clap::Values>) -> Headers {
                 headers.add(splits[0], splits[1]);
             }
             headers
-        },
-        None => Headers::new()
+        }
+        None => Headers::new(),
     }
 }
 
@@ -82,11 +82,12 @@ fn json_headers(headers_json: Option<&str>) -> Result<Option<Headers>, Error> {
         Some(value) => {
             let json_string = match value.ends_with(".json") {
                 true => read_file(value)?,
-                false => value.to_string()
+                false => value.to_string(),
             };
-            let map: std::collections::HashMap<String, String> = serde_json::from_str(&json_string)?;
+            let map: std::collections::HashMap<String, String> =
+                serde_json::from_str(&json_string)?;
             Ok(Some(Headers::from(map)))
-        },
+        }
         None => return Ok(None),
     }
 }
@@ -98,7 +99,11 @@ fn read_file(path: &str) -> Result<String, Error> {
     Ok(buf)
 }
 
-fn collect_body(body_option: Option<&str>, json_option: Option<&str>, headers: &mut Headers) -> Result<Option<String>, Error> {
+fn collect_body(
+    body_option: Option<&str>,
+    json_option: Option<&str>,
+    headers: &mut Headers,
+) -> Result<Option<String>, Error> {
     let mut body: Option<String> = None;
     if let Some(body_str) = body_option {
         body = Some(body_str.to_string());
@@ -108,7 +113,7 @@ fn collect_body(body_option: Option<&str>, json_option: Option<&str>, headers: &
             Ok(_) => {
                 body = Some(body_str.to_string());
                 headers.add("Content-Type", "application/json");
-            },
+            }
             Err(why) => error!(&why.to_string()),
         };
     }
@@ -131,48 +136,31 @@ fn get_method(method: &str) -> Method {
         "connect" => Method::CONNECT,
         "options" => Method::OPTIONS,
         "trace" => Method::TRACE,
-        "head" => Method::HEAD, 
+        "head" => Method::HEAD,
         _ => Method::GET,
     }
 }
 
 fn use_clap(args: &[String]) -> ArgMatches {
-    return
-    clap::app_from_crate!(crate_name!())
+    return clap::app_from_crate!(crate_name!())
         .version(crate_version!())
         .author(crate_authors!("\n"))
         .arg(
             Arg::new("verbose")
                 .long("verbose")
                 .short('v')
-                .help("Full request and response output in JSON")
+                .help("Full request and response output in JSON"),
         )
-        .arg(
-            Arg::new("info")
-                .long("info")
-                .help("Show info logging")
-        )
-        .arg(
-            Arg::new("url")
-                .required(true)
-                .takes_value(true)
-        )
+        .arg(Arg::new("info").long("info").help("Show info logging"))
+        .arg(Arg::new("url").required(true).takes_value(true))
         .arg(
             Arg::new("method")
                 .takes_value(true)
                 .short('m')
                 .default_value("get")
                 .possible_values(&[
-                    "get",
-                    "post",
-                    "put",
-                    "trace",
-                    "patch",
-                    "delete",
-                    "head",
-                    "options",
-                    "connect"
-                    ])
+                    "get", "post", "put", "trace", "patch", "delete", "head", "options", "connect",
+                ]),
         )
         .arg(
             Arg::new("header")
@@ -180,43 +168,37 @@ fn use_clap(args: &[String]) -> ArgMatches {
                 .takes_value(true)
                 .short('h')
                 .long("header")
-                .multiple_occurrences(true)
+                .multiple_occurrences(true),
         )
         .arg(
             Arg::new("headers")
                 .help("Headers as a json string or file ending with .json")
                 .takes_value(true)
-                .long("headers")
+                .long("headers"),
         )
         .arg(
             Arg::new("body")
                 .help("Body for request")
                 .long("body")
                 .conflicts_with("json")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             Arg::new("json")
                 .help("Send body with Content-Type:application/json")
                 .long("json")
                 .conflicts_with("body")
-                .takes_value(true)
-        )
-        .arg(
-            Arg::new("query-header")
-                .help("Query for a specific header from the response")
-                .long("query-header")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             Arg::new("no-body")
                 .help("Don't print response body")
-                .long("no-body")
+                .long("no-body"),
         )
         .arg(
             Arg::new("no-proxy")
                 .help("Do not proxy request")
-                .long("no-proxy")
+                .long("no-proxy"),
         )
         .get_matches_from(args);
 }
--- src/cli/output.rs
+++ src/cli/output.rs
@@ -1,37 +1,31 @@
-use crate::http::{headers::Headers, response::Response};
 use crate::error::Error;
+use crate::http::response::Response;
 
 pub struct Output {
     pub verbose: bool,
-    pub query_header: Option<String>,
     pub no_body: bool,
 }
 
-pub fn handle_output(mut response: Response, request: serde_json::Value, output: Output) -> Result<(), Error> {
+pub fn handle_output(
+    mut response: Response,
+    request: serde_json::Value,
+    output: Output,
+) -> Result<(), Error> {
     if output.verbose {
         if output.no_body {
-            response.body = None; 
+            response.body = None;
         }
         let json_output = serde_json::json!({"request": request, "response":response});
         println!("{}", serde_json::to_string_pretty(&json_output)?);
-    } else if let Some(h) = output.query_header {
-        query_header(&h, response.headers)
     } else {
         match &response.body {
-            Some(body) => if !output.no_body { println!("{}", body) },
-            None => {},
+            Some(body) => {
+                if !output.no_body {
+                    println!("{}", body)
+                }
+            }
+            None => {}
         }
     }
     Ok(())
-}
-
-fn query_header(header: &str, headers: Headers) {
-    let h = header.to_lowercase();
-    for (key, value) in headers {
-        if h == key.to_lowercase() {
-            for val in value {
-                println!("{}", val);
-            }
-        }
-    }
 }
